212b15c55dd8e5cbb83279008564e6bfdaf6d08e
Ahmed Eladl <ahmed.eladl@rub.de>
2023-06-24 04:04:13 +0200

    Review: Configure the data collection #5 Author: Ahmed Eladl Description: Add a new activity for configuring the data collection form the accelerometer and the location

---
+++ app/src/main/java/com/example/jien/DataCollectionActivity.java
@@ -0,0 +1,159 @@
+package com.example.jien;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+
+public class DataCollectionActivity extends AppCompatActivity {
+    private ListView optionsListView;
+    private ArrayAdapter<String> optionsAdapter;
+    private SharedPreferences sharedPreferences;
+    private SensorManager sensorManager;
+    private LocationManager locationManager;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    private String[] dataCollectionOptions = {
+            "Accelerometer",
+            "Location",
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_collection);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        optionsListView = findViewById(R.id.options_list);
+        optionsAdapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.text1, dataCollectionOptions) {
+            @Override
+            public View getView(final int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                CheckBox checkBox = view.findViewById(R.id.checkbox);
+                checkBox.setChecked(sharedPreferences.getBoolean(dataCollectionOptions[position], false));
+                checkBox.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        CheckBox checkBox = (CheckBox) v;
+                        boolean isChecked = checkBox.isChecked();
+                        sharedPreferences.edit().putBoolean(dataCollectionOptions[position], isChecked).apply();
+
+                        if (dataCollectionOptions[position].equals("Accelerometer")) {
+                            if (isChecked) {
+                                // Enable accelerometer
+                                sensorManager.registerListener(accelerometerListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+                            } else {
+                                // Disable accelerometer
+                                sensorManager.unregisterListener(accelerometerListener);
+                            }
+                        } else if (dataCollectionOptions[position].equals("Location")) {
+                            if (isChecked) {
+                                // Check location permissions and request updates
+                                requestLocationPermissions();
+                            } else {
+                                // Disable location updates
+                                locationManager.removeUpdates(locationListener);
+                            }
+                        }
+                    }
+                });
+                return view;
+            }
+        };
+
+        optionsListView.setAdapter(optionsAdapter);
+        optionsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Handle item click if needed
+            }
+        });
+    }
+
+    // Accelerometer sensor listener
+    private final SensorEventListener accelerometerListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            // Handle accelerometer sensor data
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // Handle accuracy changes if needed
+        }
+    };
+
+    // Location listener
+    private final LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            // Handle location updates
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // Handle status changes if needed
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // Handle provider enabled if needed
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // Handle provider disabled if needed
+        }
+    };
+
+    private void requestLocationPermissions() {
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+        ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, you can now call requestLocationUpdates()
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
+            } else {
+                // Permission denied, handle accordingly (e.g., display a message or disable location feature)
+            }
+        }
+    }
+}
+
