508a827a548e481ce70761e57cf034712fbe77b6
JinCheikhAhmad <131556861+JinCheikhAhmad@users.noreply.github.com>
2023-07-08 14:00:47 +0200

    WaterReminderDisplay XML and Implementation is updated

--- app/src/main/java/com/example/jien/WaterReminderDisplay.java
+++ app/src/main/java/com/example/jien/WaterReminderDisplay.java
@@ -6,14 +6,17 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.w3c.dom.Text;
 
 public class WaterReminderDisplay extends AppCompatActivity {
     FloatingActionButton addButton ;
     FloatingActionButton addGoal ;
-    EditText recordEditText;
-    EditText recordGoal;
+    TextView recordEditText;
+    TextView recordGoal;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_water_reminder_display);
@@ -21,26 +24,16 @@ public class WaterReminderDisplay extends AppCompatActivity {
         recordEditText=findViewById(R.id.record);
         addGoal=findViewById(R.id.ptntarget);
         recordGoal=findViewById(R.id.targetxt);
-        final int[] clickCount = {0};
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addButton.setPressed(true);
-                recordEditText.setEnabled(true);
-                recordEditText.requestFocus();
-                clickCount[0]++; // Increment the click count
 
-                // Update the record EditText with the click count
-                recordEditText.setText(String.valueOf(clickCount[0]));
 
             }
         });
         addGoal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addGoal.setPressed(true);  // Set the pressed state to display the click effect
-               recordGoal.setEnabled(true);  // Enable the EditText for text input
-                recordGoal.requestFocus();
 
             }
         });
