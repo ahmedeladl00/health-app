618d85053b40acef582b239ec02f17557f8c5ee2
Ahmed Eladl <ahmed.eladl@rub.de>
2023-07-09 00:01:51 +0200

    Review: Drinking amount of water #26 Author: Ahmed Eladl Description: Edited the calss of the Water so that it has a shared prefernce variable and can save the goal and the consumtion amount for the same day and to be reset at the next day. Edited the activity itself so that the user can click on the button of - Goal: and let a self designed dialogbox be show and the variabled be saved also on the water instance and shared preference and also be show at the begin of the activity. - Add: the user can add amount of water through a self design dialogbox also, that he drank and it also be set at the instance of the water and saved at the preferences and also be calculated how much should the user drink to reach his goal. At the end the variables be calculated and then be shown on the activity every thing on its place.

--- app/src/main/java/com/example/jien/Water.java
+++ app/src/main/java/com/example/jien/Water.java
@@ -1,40 +1,79 @@
 package com.example.jien;
 
-public class Water {
-    private static Water instance;
-    private int goal;
-    private int consumed;
+import android.content.Context;
+import android.content.SharedPreferences;
+import java.util.Calendar;
 
-    private Water(int goal) {
-        this.goal = goal;
-        this.consumed = 0;
+public class Water {
+    private static final String PREF_NAME = "WaterPref";
+    private static final String KEY_GOAL = "goal";
+    private static final String KEY_CONSUMED = "consumed";
+    private static final String KEY_LAST_RESET_TIMESTAMP = "lastResetTimestamp";
+
+    private static Water instance;
+    private SharedPreferences preferences;
+
+    private Water(Context context) {
+        preferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
     }
 
-    public static Water getInstance() {
+    public static Water getInstance(Context context) {
         if (instance == null) {
-            instance = new Water(0);
+            instance = new Water(context);
         }
         return instance;
     }
 
     public int getGoal() {
-        return goal;
+        return preferences.getInt(KEY_GOAL, 0);
     }
 
     public void setGoal(int goal) {
-        this.goal = goal;
+        preferences.edit().putInt(KEY_GOAL, goal).apply();
     }
 
     public int getConsumed() {
-        return consumed;
+        return preferences.getInt(KEY_CONSUMED, 0);
     }
 
-    public void addWater(int amount) {
-        consumed += amount;
+    public void addWater(Context context, int amount) {
+        int consumed = getConsumed() + amount;
+        preferences.edit().putInt(KEY_CONSUMED, consumed).apply();
+        saveResetTimestampIfNeeded(context);
     }
 
     public int getRemaining() {
-        return goal - consumed;
+        return getGoal() - getConsumed();
+    }
+
+    public void resetConsumption(Context context) {
+        preferences.edit().putInt(KEY_CONSUMED, 0).apply();
+        saveResetTimestamp(context);
+    }
+
+    private long getLastResetTimestamp() {
+        return preferences.getLong(KEY_LAST_RESET_TIMESTAMP, 0);
+    }
+
+    private void saveResetTimestamp(Context context) {
+        long currentTimestamp = Calendar.getInstance().getTimeInMillis();
+        preferences.edit().putLong(KEY_LAST_RESET_TIMESTAMP, currentTimestamp).apply();
+    }
+
+    private void saveResetTimestampIfNeeded(Context context) {
+        long lastResetTimestamp = getLastResetTimestamp();
+        long currentTimestamp = Calendar.getInstance().getTimeInMillis();
+        if (!isSameDay(lastResetTimestamp, currentTimestamp)) {
+            resetConsumption(context);
+        }
+    }
+
+    private boolean isSameDay(long timestamp1, long timestamp2) {
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal1.setTimeInMillis(timestamp1);
+        cal2.setTimeInMillis(timestamp2);
+        return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR)
+                && cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR);
     }
 }
-
