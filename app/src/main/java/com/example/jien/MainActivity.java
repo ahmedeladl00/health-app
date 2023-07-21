324b22a5f27716e4abce445a741a8f105992c98c
Nour Sayid Alo <nour.sayidalo@ruhr-uni-bochum.de>
2023-07-21 20:42:02 +0200

    Implementing new Feature to choose video from the studio and have the ability to rename the selected video

--- app/src/main/java/com/example/jien/MainActivity.java
+++ app/src/main/java/com/example/jien/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         logInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, WaterReminderActivity.class);
+                Intent intent = new Intent(MainActivity.this, InterventionRecordActivity.class);
                 startActivity(intent);
             }
         });
