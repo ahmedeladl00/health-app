48dbaebaded4976465c285e989f559e4ea14bdc1
Wardan Alshirazi Alsabbagh <129858716+WardanSY@users.noreply.github.com>
2023-07-22 02:16:52 +0200

    Just Temp. UI update dont merge

--- app/src/main/java/com/example/jien/StartPageActivity.java
+++ app/src/main/java/com/example/jien/StartPageActivity.java
@@ -2,7 +2,10 @@ package com.example.jien;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartPageActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class StartPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+        Button logInBtn = findViewById(R.id.logInBtn);
+
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartPageActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button signUpBtn = findViewById(R.id.signUpBtn);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartPageActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
     }
 }
