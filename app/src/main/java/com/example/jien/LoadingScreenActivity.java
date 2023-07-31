642c4f76b03e23c66e110391161a7d0576877280
Ahmed Eladl <ahmed.eladl@rub.de>
2023-07-31 02:18:03 +0200

    Review: User login and save language Author: Ahmed Eladl Description: - Edited the login so that if the user is already signed up then he don   need to sign up again as log as he dont need to make a new user - Saved the laguage at the user settings so that the user dont need to   change it everytime he opens the application

--- app/src/main/java/com/example/jien/LoadingScreenActivity.java
+++ app/src/main/java/com/example/jien/LoadingScreenActivity.java
@@ -2,15 +2,29 @@ package com.example.jien;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
+
+import java.util.Locale;
 
 public class LoadingScreenActivity extends AppCompatActivity {
     Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        User.getInstance().loadUserData(this);
+        String savedLanguage = User.getInstance().getLanguage();
+
+        // Apply the saved language if it exists
+        if (!savedLanguage.isEmpty()) {
+            setLocal(this, savedLanguage);
+        }
+
+        setContentView(R.layout.activity_start_page);
         setContentView(R.layout.activity_loading_screen);
 
         handler = new Handler();
@@ -19,5 +33,12 @@ public class LoadingScreenActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         },2000);
+    }
+    public void setLocal(Activity activity, String langCode){
+        Locale locale= new Locale(langCode);
+        Resources resources = activity.getResources();
+        Configuration config = resources.getConfiguration();
+        config.setLocale(locale);
+        resources.updateConfiguration(config,resources.getDisplayMetrics());
     }
 }
--- app/src/main/java/com/example/jien/MainActivity.java
+++ app/src/main/java/com/example/jien/MainActivity.java
@@ -37,17 +37,19 @@ public class MainActivity extends AppCompatActivity {
             String password = editTextPassword.getText().toString();
             User user = User.getInstance();
 
-            if (user.isValidLogin(email, password)) {
-                Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
-                startActivity(intent);
-            } else {
-                if (TextUtils.isEmpty(user.getEmail()) || TextUtils.isEmpty(user.getPassword())) {
-                    Toast.makeText(MainActivity.this, (R.string.you_haven_t_signed_up_yet_please_sign_up_first), Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, (R.string.invalid_credentials_please_try_again), Toast.LENGTH_SHORT).show();
-                }
-            }
+            user.loadUserData(MainActivity.this);
 
+            if (!TextUtils.isEmpty(user.getEmail()) && !TextUtils.isEmpty(user.getPassword())) {
+                if (user.isValidLogin(email, password)) {
+                    Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(MainActivity.this, R.string.invalid_credentials_please_try_again, Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(MainActivity.this, R.string.you_haven_t_signed_up_yet_please_sign_up_first, Toast.LENGTH_SHORT).show();
+            }
         });
 
     }
